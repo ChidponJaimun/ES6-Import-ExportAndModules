e6d9b470b0f31f20f8793f8a68e12e8b5d218ace
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-10-21 01:01:34 +0700

    2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pi, { doublePi, triplePi } from "./math.js";
+import * as pi from "./math.jsx"; //this way for faster
+// import pi2, { doublePi, triplePi } from "./math.jsx"; prefer this way for esy error checking.
 
 ReactDOM.render(
   <ul>
-    <li>{pi}</li>
-    <li>{doublePi()}</li>
-    <li>{triplePi()}</li>
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
